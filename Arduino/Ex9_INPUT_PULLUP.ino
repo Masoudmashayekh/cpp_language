6143d6f937485c70220a656c18f4c97310cae981
masoud mashayekh <masoudmashayekh4944@gmail.com>
2025-05-16 20:15:29 +0200

    Add files via upload

---
+++ Arduino/Ex9_INPUT_PULLUP.ino
@@ -0,0 +1,17 @@
+int LED=12;
+int in1=2;
+int int2=4;
+
+void setup()
+ {
+ pinMode(LED,OUTPUT);
+ pinMode(in1,INPUT_PULLUP);
+ pinMode(in2,INPUT_PULLUP);
+}
+
+void loop() 
+{
+ if (digitalRead(in1)==LOW)
+ {D}
+
+}
