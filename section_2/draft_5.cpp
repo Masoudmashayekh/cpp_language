0c456d14c9e1841f43483a206ce5842d8746de13
Masoud <masoudmashayekh4944@gmail.com>
2026-04-24 17:11:01 +0200

    save

--- section_2/draft_5.cpp
+++ section_2/draft_5.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
     std::cout << "Tell me your age: ";
+    std::string name {};
     int age {};
-    std::cin >> age;
-    std::cout << "Your age is "<< age << ".\n";
+    
+    std::cin >> name >> age;
+    std::cout << "Hi " << name << ". Your age is "<< age << ".\n";
     return 0;
 }
