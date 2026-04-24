c44b237a96b2cf9f99b741e8ecc5dd6a9efce370
Masoud <masoudmashayekh4944@gmail.com>
2026-04-24 16:43:04 +0200

    save

---
+++ section_2/draft_4.cpp
@@ -0,0 +1,10 @@
+# include <iostream> // io=> input output
+
+int main()
+{
+    int age {5};
+    int name {};
+    std::cout << age << std::endl; // std::cout << for print and use std::endl for going next line
+    std::cout << "You are " << age << " years old.";
+    return 0;
+}
