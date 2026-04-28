562de3337b1fbdcc004fa17bd581cf6a58d0c452
Masoud <masoudmashayekh4944@gmail.com>
2026-04-28 11:48:00 +0200

    save

--- section_3/draft_9.cpp
+++ section_3/draft_9.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #define PRINT_JOE
+#define FOO 9
 
 int main()
 {
@@ -11,5 +12,10 @@ int main()
     #ifndef PRINT_BOB
     std::cout << "Bob not define" << "\n";
     #endif
+
+    #ifdef FOO
+    std::cout << FOO << "\n";
+    #endif
+
     return 0;
 }
